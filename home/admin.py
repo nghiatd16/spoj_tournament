21f79224059fbeb84907db7ddc9c050c8da307a8
Tran Dai Nghia <nghiatd.proptit@gmail.com>
2019-02-03 20:25:59 +0700

    Add admin site

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Member
 # Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['id','full_name','username','grade', 'num_solved', 'score', 'target']
+    list_per_page = 15
+    search_fields = ['full_name', 'username']
+    list_editable = ['target']
+    sortable_by = ['target', 'score']
+admin.site.register(Member, MemberAdmin)
